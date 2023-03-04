1029a570f48b053b1a1e8eb9a28552f3034cd96b
cosin2077 <bermasunita6@gmail.com>
2023-03-04 18:46:19 +0800

    feat: pre check

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './main/pre'
 import './main/check'
 import './main/init'
 import { runService } from './main/run'
---
+++ src/main/pre.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import fse from "fs-extra";
+import path from "path";
+dotenv.config();
+import { debug } from "./init";
+import { appConfigPath, appLogPath } from "./../constants/index";
+
+function ensureHomeDirectory() {
+  fse.ensureDirSync(appLogPath);
+}
+ensureHomeDirectory();
+if (process.env.OPEN_AI_KEY) {
+  // .env in .cwd, copy to appHome
+  const envFile = path.resolve(__dirname, ".env");
+  const destEnvPath = path.resolve(appConfigPath, ".env");
+  if (fse.existsSync(envFile)) {
+    fse.copySync(envFile, destEnvPath, { overwrite: true });
+  }
+} else {
+  debug("OPEN_AI_KEY needed!");
+  process.exit(1);
+}
