ce0cacb965d7832176711ac58038bd7751fb51fc
cosin2077 <bermasunita6@gmail.com>
2023-03-04 18:07:07 +0800

    feat: updagte

--- src/main/run/index.ts
+++ src/main/run/index.ts
@@ -5,7 +5,7 @@ import { runCommandLineService } from "./command";
 
 
 export const runService = async () => {
-  if (argv._.length === 0) {
+  if (argv._.length === 0 && Object.keys(argv).length === 1) {
     await runCommandLineService()
   }
   if (argv.web) {
--- src/main/run/web.ts
+++ src/main/run/web.ts
@@ -1,3 +1,4 @@
+import { parse as dotenvParse } from "dotenv";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import path from "path";
 import { logger } from "../../logger";
@@ -45,16 +46,25 @@ async function getProjectDir() {
   const projectPath = path.resolve(__dirname, "..", "..", "..");
   return projectPath;
 }
+const exposeEnv = ["OPEN_AI_KEY"];
 async function copyEnv(from: string, to: string) {
   const fromEnv = path.resolve(from, ".env");
   const toEnv = path.resolve(to, ".env");
   const checkFrom = await existsSync(fromEnv);
+
   if (!checkFrom) {
     debug(`error: .env not exists!`);
     logger.fatal(`error: .env not exists!${fromEnv}`);
     return;
   }
   const content = readFileSync(fromEnv, "utf-8");
-  writeFileSync(toEnv, content, "utf-8");
+  const parsed = dotenvParse(content);
+  let newContent = "";
+  for (let prop in parsed) {
+    if (exposeEnv.includes(prop)) {
+      newContent += `VITE_${prop}=${parsed[prop]}\n`;
+    }
+  }
+  writeFileSync(toEnv, newContent, "utf-8");
   debug(`copy .env file to ${toEnv} succeed!`);
 }
